e90590c40fe5251e2ec219da8289053d5bd30271
Aman Bhai Patel <amanpatel2000@yahoo.com>
2020-01-11 10:18:34 +0530

    2nd commit
    -Removed ID_List.dat
    -Added ID_List.txt
    -Added Register() and show all users to the program.
    -Still need to fix backspace on password input
    Please enter the commit message for your changes. Lines starting

--- Program/program.cpp
+++ Program/program.cpp
@@ -5,39 +5,115 @@
 #include <conio.h>
 using namespace std;
 
+string USER_ID;
+string PASSWORD;
+int type;
+
+void DD(string a, string b, int c)
+{
+  cout << "\nDetails:";
+  cout << "\nUSERNAME:" << a;
+  cout << "\nPassword:" << b;
+  cout << "\nUser Type:" << c;
+}
+
+void Register()
+{
+  cout << "\nRegistering has begun";
+ A1:
+  cout << "\nEnter a username:";
+  cin >> USER_ID;
+  fstream op("../Data/ID_List.txt",ios::in);
+  if(!op)
+    {
+      cout << "\nFile Not Found!";
+      cout << "\nError Code 100";
+      cout << "\nProgram Aborting";
+      throw(100);
+    }
+  while(!op.eof())
+  {
+    string UID,PWD;
+    op >> UID;
+    if(UID==USER_ID)
+      {
+	op.close();
+	cout << endl << "This user ID has been taken, Please enter a new ID:";
+	goto A1;
+      }
+  }
+  op.close();
+  cout << "\nUsername Is available";
+  cout << "\nPlease Enter a password:";
+  char c;
+  while(c!=13)
+    {
+      c=(char)getch();
+      if(c==13)
+	break;
+      if(c==8)
+	continue;
+      PASSWORD+=c;
+      cout << "*";
+    }
+  cout << "\nEnter a user type:\n1.Admin\n2.Owner\n3.Accountant\n4.Receptionist\n5.Gym Client:";
+  cin >> type;
+  DD(USER_ID,PASSWORD,type);
+  cout << endl;
+  fstream op1("../Data/ID_List.txt",ios::out|ios::app);
+  op1 << USER_ID << "\t" << PASSWORD << "\t" << type << "\n";
+  op1.close();
+}
+void ListUsers()
+{
+  string UID,PWD;
+  int t;
+  fstream login("../Data/ID_List.txt",ios::in);
+  if(!login)
+    {
+      cout << "\nFile Not Found!";
+      cout << "\nError Code 100";
+      cout << "\nProgram Aborting";
+    }
+  else
+    {
+      cout << "\n List of users registered:\n";
+      while(true)
+	{
+	  login >> UID >> PWD >> t;
+	  if(login.eof())
+	    break;
+	  DD(UID,PWD,t);
+	  cout << endl;
+	}
+    }
+  login.close();
+}
+
 int main()
 {
-	try
+  while(1)
+    {
+      cout << "\n--------------------Program Menu--------------------";
+      cout << "\n--Choose an option:----";
+      cout << "\n1. Register";
+      //cout << "\n2. Login";
+      cout << "\n3. Show Users List";
+      cout << "\n4. Exit";
+      cout << endl;
+      int choice;
+      cin >> choice;
+      switch(choice)
 	{
-	        fstream login("ID_List.dat",ios::in|ios::out|ios::binary);
-		if(!login)
-		{
-			cout << "File Not Found!";
-			cout << "Program will now exit";
-			throw(100);
-		}
-		cout << "Enter Your Login ID:\n";
-		string USER_ID;
-		getline(cin,USER_ID);
-		cout << "Enter Password:\n";
-		string PASSWORD;
-		char c=11;
-		while(c!=13)
-		  {
-		    c=(char)getch();
-		    if(c==13)
-		      break;
-		    PASSWORD+=c;
-		    cout << "*";
-		  }
-		cout << "\nUserID:" << USER_ID << "\nPassword:" << PASSWORD;		  
-		login.close();
+	case 1:
+	  Register();
+	  break;
+	case 3:
+	  ListUsers();
+	  break;
+	case 4:
+	  exit(0);
 	}
-	catch(int x)
-	{
-		if(x==100)
-			exit(0);
-		
-	}
-	return 0;
+    }
+  return 0;
 }

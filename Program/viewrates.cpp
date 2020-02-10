5f01c26583fbaaf37b1a9b58ee45cc3d4341cbf2
Aman Bhai Patel <amanpatel2000@yahoo.com>
2020-02-10 12:16:48 +0530

    Fixed View Rates not shiwing dietician rates

--- Program/viewrates.cpp
+++ Program/viewrates.cpp
@@ -22,7 +22,8 @@ void VRates()
 		cout << "\n   Type\\Time span\t\tMonthly\t\t3months\t\t6months\t\t12months";
 		fin >> mr >> qr >> hyr >> yr >> gtrmr >> gtrqr >> gtrhyr >> gtryr >> drmr >> drqr >> drhyr >> dryr;
 		cout << "\n   Membership Fees \t\t" << mr << "\t\t" <<  qr << "\t\t" << hyr << "\t\t" << yr;
-		cout << "\n   Gym Trainer Fees\t\t" << gtrmr << "\t\t" << gtrqr << "\t\t" << gtrhyr << "\t\t" << gtryr << endl;
+		cout << "\n   Gym Trainer Fees\t\t" << gtrmr << "\t\t" << gtrqr << "\t\t" << gtrhyr << "\t\t" << gtryr;
+		cout << "\n   Dietician Fees  \t\t" << drmr << "\t\t" << drqr << "\t\t" << drhyr << "\t\t" << dryr << endl;
 		fin.close();
 		cout << "To return to menu : ";
 		system("Pause");

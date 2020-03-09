ae2b54bce0091bedbd7eaf147c0823ecdf6ca1d7
Aman Bhai Patel <amanpatel2000@yahoo.com>
2020-03-09 19:00:08 +0530

    Undoing rebase

--- ProgramPy/encryptor.py
+++
@@ -1,6 +0,0 @@
-def encryptDecrypt(inpString): 
-    xorKey = 'P'; 
-    length = len(inpString);  
-    for i in range(length): 
-        inpString = (inpString[:i] + chr(ord(inpString[i]) ^ ord(xorKey)) + inpString[i + 1:]);
-    return inpString; 
